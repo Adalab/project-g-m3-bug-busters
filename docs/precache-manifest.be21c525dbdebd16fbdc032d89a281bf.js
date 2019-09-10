ba1f0f2d3677beaba13a5eb4c2a52609ecbecee1
martagrio <marta.igr90@gmail.com>
2019-09-10 23:18:55 +0200

    Run npm build docs

---
+++ docs/precache-manifest.be21c525dbdebd16fbdc032d89a281bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6a764ddca2482e81fb445252721b00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc4fe7f3f9e083f9fa4c",
+    "url": "./static/css/main.c8410477.chunk.css"
+  },
+  {
+    "revision": "11b337ad19bf373b9518",
+    "url": "./static/js/2.f31f391a.chunk.js"
+  },
+  {
+    "revision": "fc4fe7f3f9e083f9fa4c",
+    "url": "./static/js/main.cfb773e7.chunk.js"
+  },
+  {
+    "revision": "995509725217c5c4acb4",
+    "url": "./static/js/runtime~main.1f79c23a.js"
+  },
+  {
+    "revision": "8cafb42b2efac0efd77e24868095f62a",
+    "url": "./static/media/logo.8cafb42b.svg"
+  }
+]);
