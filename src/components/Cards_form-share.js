2db7040f34634a28d8ec08166489239117c8d088
martagrio <marta.igr90@gmail.com>
2019-09-16 22:41:08 +0200

    Fix id to third instead of second on div from cards-form-share

--- src/components/Cards_form-share.js
+++ src/components/Cards_form-share.js
@@ -8,7 +8,7 @@ class CardsFormShare extends React.Component {
   render() {
     return (
       <div className={`js__submit-container ${this.props.addClass(this.props.id, 'third')}`} id = "third">
-        <div className="submit-share js__collapsable" onClick={this.props.divAction} id="second">
+        <div className="submit-share js__collapsable" onClick={this.props.divAction} id="third">
           <h2 className="submit-share form-title title-3">comparte</h2>
         </div>
         <div className= {`submit-container collapsable-container`} data-id = "third">
